77c7e203d32c9eba8b73b08ebd49227cf64c7ef5
tima <tima@timatekiMacBook-Pro.local>
2018-10-10 22:07:07 +1100

    update carema

--- app/src/main/java/com/example/d/instagramclone/CameraFragment.java
+++ app/src/main/java/com/example/d/instagramclone/CameraFragment.java
@@ -1,22 +1,49 @@
 package com.example.d.instagramclone;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class CameraFragment extends Fragment {
+
+    ImageView imageView;
+    Button buttonCamera;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_camera, container,false);
 
+        buttonCamera = buttonCamera.findViewById(R.id.buttonCamera);
+        imageView = imageView.findViewById(R.id.imageView);
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+                //intent.putExtra(MediaStore.EXTRA_OUTPUT, )
+            }
+        });
         return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView .setImageBitmap(bitmap);
+    }
 
 
-}
+    }
